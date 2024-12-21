35da0d332e876f51cfec560684fddb0b1775662f
gusthegray <gusthegray@gmail.com>
2024-12-21 15:25:14 -0800

    day01a complete

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,3 +1,62 @@
+use std::{fs::read_to_string, usize};
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_lines("input.txt");
+
+    let answer = find_solution(input);
+    println!("Answer is: {}", answer)
+    
+}
+
+
+fn find_solution(input: Vec<String>) -> usize {
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+
+    for l in input{
+        let vals: Vec<&str> = l.split(' ').collect();
+        println!("vals {:?}", vals);
+        left.push(vals[0].parse::<usize>().unwrap());
+        right.push(vals[vals.len()-1].parse::<usize>().unwrap());
+    }
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let mut total_distance = 0;
+    for (l, r) in left.iter().zip(right.iter()) {
+        let distance = if l > r {l - r } else {r-l};
+        total_distance += distance
+    }
+
+    total_distance
+}
+
+
+
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+    .expect("Could not find file").lines().map(String::from).collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_solution() {
+        let sample_input = vec![
+            "3 4".to_string(),
+            "4 3".to_string(),
+            "2 5".to_string(),
+            "1 3".to_string(),
+            "3 9".to_string(),
+            "3 3".to_string(),
+        ];
+
+        let result = find_solution(sample_input);
+        assert_eq!(result, 11, "Expected distance is 11");
+    }
 }
