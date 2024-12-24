246efdb364d03daed01134c65f081630ff5cccaa
gusthegray <gusthegray@gmail.com>
2024-12-24 09:00:37 -0800

    day03a done

--- day03a/src/main.rs
+++ day03a/src/main.rs
@@ -1,7 +1,8 @@
+use regex::Regex;
 use shared_lib::{print_answer, read_lines};
 
 fn main() {
-    let input = read_lines("inputs/day02.txt");
+    let input = read_lines("inputs/day03.txt");
 
     let answer = find_solution(input);
 
@@ -10,8 +11,30 @@ fn main() {
 
 fn find_solution(input: Vec<String>) -> usize {
 
-    0
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
+    let mut matches: Vec<String> = Vec::new();
+
+    for s in input {
+        let m: Vec<String> = re.find_iter(&s).map(|m| m.as_str().to_string()).collect();
+        matches.extend(m);
+    }
+    let mut total =0;
+
+    for s in matches {
+        let s = s[4..].to_string();
+        let s = s[..s.len()-1].to_string();
+
+        let nums: Vec<usize> = s.split(',').map(|x| x.parse::<usize>().unwrap()).collect();
+        total += nums[0] * nums[1];
+        println!("{}", s);
+    }
+
+
+
+    total
 }
+
+
 
 #[cfg(test)]
 mod tests {
